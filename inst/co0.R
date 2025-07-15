04772f1ca65db7fbd01632a019166bd1ece2a824
Erik Volz <erik.volz@gmail.com>
2025-07-16 00:00:16 +0100

    print

--- inst/co0.R
+++ inst/co0.R
@@ -3,6 +3,9 @@ library( mpoxsmcam.app )
 cargs <- commandArgs(trailingOnly = TRUE )
 dxs <- as.numeric( cargs[1] )
 
+cat( '======================\n') 
+print( dxs )
+
 o = .compute_elasticities(  dxs = dxs, Np = 5e4, nrep = 7, ncpu = 8)
 
 write.csv( o , file = glue::glue('elasticity0-{cargs[1]}.csv' ))
